2097bba8b16c4fa46911a1517acf5b3609cdefcd
masterh4cker <tabathafoes@gmail.com>
2020-03-06 14:38:19 -0700

    3 files modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
 
     constructor() {
         this.products = [
-            { id: 'p01', name: 'name 1', price: 100, photo: 'download.jfif' },
+            { id: 'p01', name: 'name 1', price: 100, photo: 'shoe1.jpg' },
             { id: 'p02', name: 'name 2', price: 200, photo: 'thumb2.gif' },
             { id: 'p03', name: 'name 3', price: 300, photo: 'thumb3.gif' }
         ];
